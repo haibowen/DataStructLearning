d7e952922981ac19722280d96f27364024da14f0
hyplo <1461154748@qq.com>
2021-12-01 22:35:33 +0800

    dev class 2

---
+++ app/src/main/java/com/example/datastructlearning/DevClass2.java
@@ -0,0 +1,10 @@
+package com.example.datastructlearning;
+
+/**
+ * Created on 2021/12/1
+ * Author: hyplo
+ * Email: haibowen088@gmail.com
+ * Description: show me the code change the world
+ */
+public class DevClass2 {
+}
