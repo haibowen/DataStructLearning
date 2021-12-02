fcffbc4bc16c5301dea45951f1c054c5cc1abe3a
hyplo <1461154748@qq.com>
2021-12-02 21:47:55 +0800

    提交linklist

---
+++ app/src/main/java/com/example/datastructlearning/LinkListDemo.java
@@ -0,0 +1,13 @@
+package com.example.datastructlearning;
+
+/**
+ * Created on 2021/12/2
+ * Author: hyplo
+ * Email: haibowen088@gmail.com
+ * Description: show me the code change the world
+ */
+public class LinkListDemo {
+    public void test() {
+        System.out.println("这是一个测试的方法");
+    }
+}
